a54def72e715d8bd9948804a77757b69242f105c
easywaldoji <easywaldoji@gmail.com>
2021-03-21 13:53:45 +0900

    DTO 에 대한 ClassValidator 추가

--- src/dto/SampleRequestDto.ts
+++ src/dto/SampleRequestDto.ts
@@ -1,4 +1,8 @@
+import { IsEmail, IsNotEmpty, Min } from 'class-validator';
 export class SampleRequestDto {
+
+    @IsNotEmpty()
     private carName : String;
+    @Min(1000)
     private salePrice : Number;
 }
